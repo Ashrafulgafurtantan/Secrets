099db6d9f69c90e524865e79a8f5a14fc3c2721d
Dexter <bsse1021@iit.du.ac.bd>
2020-07-16 04:12:13 +0600

    added signIn with google

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 var lodash = require("lodash");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
+const findOrCreate = require("mongoose-findorcreate");
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -42,20 +43,65 @@ const userSchema = new mongoose.Schema({
     type: String,
     require: true,
   },
+  googleId: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/secrets",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      console.log(profile.displayName);
+      User.findOrCreate(
+        { googleId: profile.id, username: profile.displayName },
+        function (err, user) {
+          return cb(err, user);
+        }
+      );
+    }
+  )
+);
 
 app.get("/", function (req, res) {
   res.render("home");
 });
 
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get(
+  "/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+  }
+);
+
+app.get("/submit", function (req, res) {
+  res.render("submit");
+});
 app.get("/secrets", function (req, res) {
   if (req.isAuthenticated()) {
     console.log("user auth");
@@ -78,38 +124,6 @@ app.get("/:path", function (req, res) {
     res.render("register");
   }
 });
-/*
-app.post("/register", function (req, res) {
-  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-    const user = new User({
-      username: req.body.username,
-      password: hash,
-    });
-    user.save(function (err) {
-      if (!err) {
-        res.render("secrets");
-      } else {
-        res.send("Registration problem");
-      }
-    });
-  });
-});
-app.post("/login", function (req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
-  User.findOne({ username: username }, function (err, founduser) {
-    if (err) {
-      console.log("Problem occured");
-    } else {
-      bcrypt.compare(password, founduser.password, function (err, result) {
-        if (result === true) {
-          res.render("secrets");
-        }
-      });
-    }
-  });
-});
-*/
 
 app.post("/register", function (req, res) {
   User.register({ username: req.body.username }, req.body.password, function (
@@ -148,5 +162,3 @@ app.post("/login", function (req, res) {
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
 });
-/*  email:"bsse1021@iit.du.ac.bd",
-    password : "01191752075", */
